d16a35b15f1f1c1b74be8bf29b6782b8a989f65d
César Olvera <olvera_cesar.m@hotmail.com>
2023-05-14 17:07:20 -0600

    Se instalo y creo archibo firebase

---
+++ preentrega1/src/firebase/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAIgZoenWBY2MlF21YL7tez1xwJnP19xSI",
+  authDomain: "proyecto-57ed2.firebaseapp.com",
+  projectId: "proyecto-57ed2",
+  storageBucket: "proyecto-57ed2.appspot.com",
+  messagingSenderId: "616357824147",
+  appId: "1:616357824147:web:382b976096e5afda8fef66"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
