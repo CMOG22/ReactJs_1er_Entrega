284af52a43fb643f16a9019405c91672df4e803d
César Olvera <olvera_cesar.m@hotmail.com>
2023-05-14 09:27:02 -0600

    Se agrego informacion a src compunents app

--- preentrega1/src/components/App.jsx
+++ preentrega1/src/components/App.jsx
@@ -6,7 +6,9 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Navbar } from './Navbar/Navbar';
 import { ItemListContainer } from './ItemListContainer/ItemListContainer';
 import { ItemDetailContainer } from './ItemDetailContainer/ItemDetailContainer';
-import { Derechos } from './Pie/Derechos/Derechos';
+import { Checkout } from './Checkout/Checkout';
+import { Cart } from './Cart/Cart';
+        import { Derechos } from './Pie/Derechos/Derechos';
 export const App = () => {
   return (
     <>
@@ -16,6 +18,8 @@ export const App = () => {
           <Route path='/' element={<ItemListContainer />} />
           <Route path='/category/:category' element={<ItemListContainer />} />
           <Route path='/product/:id' element={<ItemDetailContainer />} />
+          <Route path='/cart' element={<Cart />} />
+          <Route path='*' element={<h1>404 Not Found</h1>} />
         </Routes>
         <Derechos />
       </BrowserRouter>
