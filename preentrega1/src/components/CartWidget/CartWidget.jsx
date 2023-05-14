38d7bdaff0cf37fd5017d0355b82c3ce5d630c49
César Olvera <olvera_cesar.m@hotmail.com>
2023-05-14 09:38:20 -0600

    Se modifico cartWidget

--- preentrega1/src/components/CartWidget/CartWidget.jsx
+++ preentrega1/src/components/CartWidget/CartWidget.jsx
@@ -1,10 +1,14 @@
-import { ClassAttributes } from "./style/cartWidget.css";
+import { ClassAttributes } from "./style/cartWidget.css"
+import { useCarritoContext } from "../../context/CartContext"
+import { Link } from "react-router-dom"
 
-export const CartWidget = ({ cantCarrito }) => {
+export const CartWidget = () => {
+  const { getItemQuantity } = useCarritoContext()
   return (
     <>
-      <button className="btn btn-dark cart">
-        <i className="fas fa-shopping-cart fa-lg">
+      <button className="btn btn-dark cartWidget cart">
+        <Link to={"/cart"} className="nav-link">
+          <i className="fas fa-shopping-cart fa-lg">
           <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-shopping-cart" width="40" height="40" viewBox="0 0 24 24" stroke-width="1.5" stroke="#000000" fill="none" stroke-linecap="round" stroke-linejoin="round">
             <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
             <circle cx="6" cy="19" r="2" />
@@ -12,9 +16,11 @@ export const CartWidget = ({ cantCarrito }) => {
             <path d="M17 17h-11v-14h-2" />
             <path d="M6 5l14 1l-1 7h-13" />
           </svg>
-        </i>
+          </i>
+          {getItemQuantity() > 0 && <span className="cantCarrito">{getItemQuantity()}</span>}
+        </Link>
       </button>
-      <p>{cantCarrito}</p>
+
     </>
   )
 }
